4fd78337a4be00dc42f4e5a89cbd15d29d705e13
Guilherme Garrote <guilhermegg2008@gmail.com>
2023-10-16 19:38:34 -0300

    Adiciona a manipulação de perguntas e respostas com Matriz

--- UMenu.pas
+++ UMenu.pas
@@ -48,15 +48,18 @@ type
     FundoSociedade: TImage;
     TextSociedade: TImage;
     BtnSociedade: TButton;
-    procedure BtnJogarClick(Sender: TObject);
+    procedure InicializarMatrizes;
     procedure BtnSaudeClick(Sender: TObject);
     procedure BtnEducacaoClick(Sender: TObject);
     procedure BtnMeioAmbienteClick(Sender: TObject);
     procedure BtnSociedadeClick(Sender: TObject);
+    procedure BtnJogarClick(Sender: TObject);
   private
   public
-    { Public declarations }
-    tema: string;
+    perguntas: array of array of string;
+    respostas: array of array of string;
+    coluna: integer;
+    linha: integer;
   end;
 
 var
@@ -68,6 +71,105 @@ implementation
 
 uses UQuestions;
 
+procedure TFrmMenu.InicializarMatrizes;
+begin
+  // Inicializar a matriz de perguntas (4 linhas e 4 colunas)
+  SetLength(perguntas, 4, 4);
+
+  // Preencher a matriz de perguntas com strings de exemplo
+  perguntas[0, 0] := 'a';
+  perguntas[0, 1] := 'a';
+  perguntas[0, 2] := 'a';
+  perguntas[0, 3] := 'a';
+
+  perguntas[1, 0] := 'a';
+  perguntas[1, 1] := 'a';
+  perguntas[1, 2] := 'a';
+  perguntas[1, 3] := 'a';
+
+  perguntas[2, 0] := 'a';
+  perguntas[2, 1] := 'a';
+  perguntas[2, 2] := 'a';
+  perguntas[2, 3] := 'a';
+
+  perguntas[3, 0] := 'a';
+  perguntas[3, 1] := 'a';
+  perguntas[3, 2] := 'a';
+  perguntas[3, 3] := 'a';
+
+  // Inicializar a matriz de respostas (16 linhas e 4 colunas)
+  SetLength(respostas, 4, 16);
+
+  // Preencher a matriz de respostas com strings de exemplo
+  respostas[0, 0] := 'a';
+  respostas[0, 1] := 'a';
+  respostas[0, 2] := 'a';
+  respostas[0, 3] := 'a';
+  respostas[0, 4] := 'a';
+  respostas[0, 5] := 'a';
+  respostas[0, 6] := 'a';
+  respostas[0, 7] := 'a';
+  respostas[0, 8] := 'a';
+  respostas[0, 9] := 'a';
+  respostas[0, 10] := 'a';
+  respostas[0, 11] := 'a';
+  respostas[0, 12] := 'a';
+  respostas[0, 13] := 'a';
+  respostas[0, 14] := 'a';
+  respostas[0, 15] := 'a';
+
+  respostas[1, 0] := 'a';
+  respostas[1, 1] := 'a';
+  respostas[1, 2] := 'a';
+  respostas[1, 3] := 'a';
+  respostas[1, 4] := 'a';
+  respostas[1, 5] := 'a';
+  respostas[1, 6] := 'a';
+  respostas[1, 7] := 'a';
+  respostas[1, 8] := 'a';
+  respostas[1, 9] := 'a';
+  respostas[1, 10] := 'a';
+  respostas[1, 11] := 'a';
+  respostas[1, 12] := 'a';
+  respostas[1, 13] := 'a';
+  respostas[1, 14] := 'a';
+  respostas[1, 15] := 'a';
+
+  respostas[2, 0] := 'a';
+  respostas[2, 1] := 'a';
+  respostas[2, 2] := 'a';
+  respostas[2, 3] := 'a';
+  respostas[2, 4] := 'a';
+  respostas[2, 5] := 'a';
+  respostas[2, 6] := 'a';
+  respostas[2, 7] := 'a';
+  respostas[2, 8] := 'a';
+  respostas[2, 9] := 'a';
+  respostas[2, 10] := 'a';
+  respostas[2, 11] := 'a';
+  respostas[2, 12] := 'a';
+  respostas[2, 13] := 'a';
+  respostas[2, 14] := 'a';
+  respostas[2, 15] := 'a';
+
+  respostas[3, 0] := 'a';
+  respostas[3, 1] := 'a';
+  respostas[3, 2] := 'a';
+  respostas[3, 3] := 'a';
+  respostas[3, 4] := 'a';
+  respostas[3, 5] := 'a';
+  respostas[3, 6] := 'a';
+  respostas[3, 7] := 'a';
+  respostas[3, 8] := 'a';
+  respostas[3, 9] := 'a';
+  respostas[3, 10] := 'a';
+  respostas[3, 11] := 'a';
+  respostas[3, 12] := 'a';
+  respostas[3, 13] := 'a';
+  respostas[3, 14] := 'a';
+  respostas[3, 15] := 'a';
+end;
+
 procedure TFrmMenu.BtnEducacaoClick(Sender: TObject);
 begin
   if ((RecSaude.Visible = True) or (RecMeioAmbiente.Visible = True) or
@@ -78,30 +180,7 @@ begin
     RecSociedade.Visible := False;
   end;
   RecEducacao.Visible := True;
-  tema := 'educacao';
-end;
-
-procedure TFrmMenu.BtnJogarClick(Sender: TObject);
-begin
-  FrmQuestions.Show;
-  if (tema = 'educacao') then
-  begin
-    FrmQuestions.RBA.Text := 'oiii';
-  end;
-
-end;
-
-procedure TFrmMenu.BtnMeioAmbienteClick(Sender: TObject);
-begin
-  if ((RecSaude.Visible = True) or (RecEducacao.Visible = True) or
-    (RecSociedade.Visible = True)) then
-  begin
-    RecSaude.Visible := False;
-    RecEducacao.Visible := False;
-    RecSociedade.Visible := False;
-  end;
-  RecMeioAmbiente.Visible := True;
-  tema := 'meioambiente';
+  coluna := 0;
 end;
 
 procedure TFrmMenu.BtnSaudeClick(Sender: TObject);
@@ -114,7 +193,20 @@ begin
     RecSociedade.Visible := False;
   end;
   RecSaude.Visible := True;
-  tema := 'saude';
+  coluna := 1;
+end;
+
+procedure TFrmMenu.BtnMeioAmbienteClick(Sender: TObject);
+begin
+  if ((RecSaude.Visible = True) or (RecEducacao.Visible = True) or
+    (RecSociedade.Visible = True)) then
+  begin
+    RecSaude.Visible := False;
+    RecEducacao.Visible := False;
+    RecSociedade.Visible := False;
+  end;
+  RecMeioAmbiente.Visible := True;
+  coluna := 2;
 end;
 
 procedure TFrmMenu.BtnSociedadeClick(Sender: TObject);
@@ -127,7 +219,15 @@ begin
     RecEducacao.Visible := False;
   end;
   RecSociedade.Visible := True;
-  tema := 'sociedade';
+  coluna := 3;
+end;
+
+procedure TFrmMenu.BtnJogarClick(Sender: TObject);
+begin
+  InicializarMatrizes;
+  FrmQuestions.Show;
+  FrmQuestions.RBA.Text := respostas[coluna, 0];
+
 end;
 
 end.
